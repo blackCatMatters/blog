8ecb9adace1460170f39de0808c93bbaa1e18138
Aude <20560604+Aud123@users.noreply.github.com>
2024-09-27 15:44:25 +0200

    fix(deploy): remove unnecessary pathPrefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,6 @@
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  pathPrefix: '/blog',
   siteMetadata: {
     title: `A Blog by a woman in tech`,
     siteUrl: `https://www.blackcatmatters.com`,
