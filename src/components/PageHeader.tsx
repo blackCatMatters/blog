4a63cfe7e52a45aa5f801336a6a09009620b7a9f
Falco <20560604+bcm-afalco@users.noreply.github>
2024-12-03 15:39:05 +0100

    feat(burger menu): add logic for close and open burger menu

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,10 +1,16 @@
 import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
-import React from 'react';
+import React, { useState } from 'react';
 import { useSiteMetadata } from '../hooks/use-site-metadata';
 
 export const PageHeader: React.FC = () => {
   const { title, navigation } = useSiteMetadata();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="bg-white">
       <div className="container mx-auto flex max-w-5xl items-center justify-between py-6">
@@ -25,21 +31,57 @@ export const PageHeader: React.FC = () => {
             </span>
           </Link>
         )}
-      </div>
-      <div className="flex w-full items-center justify-between text-sm sm:w-auto">
-        <nav>
-          <ul className="flex justify-between px-2 sm:px-0">
+
+        {/* Desktop Navigation */}
+        <nav className="hidden md:flex">
+          <ul className="flex space-x-4">
             {navigation?.map(
               (nav) =>
                 nav?.path && (
-                  <li key={nav.path} className="px-2 sm:px-4">
-                    <Link to={nav.path}>{nav.name}</Link>
+                  <li key={nav.path}>
+                    <Link to={nav.path} className="hover:text-gray-600">
+                      {nav.name}
+                    </Link>
                   </li>
                 ),
             )}
           </ul>
         </nav>
+
+        {/* Mobile Burger Menu */}
+        <button
+          className="block focus:outline-none md:hidden"
+          onClick={toggleMenu}
+          aria-label="Toggle Menu"
+        >
+          {isMenuOpen ? (
+            <span className="text-2xl font-bold">x</span>
+          ) : (
+            <span className="text-2xl">☰</span>
+          )}
+        </button>
       </div>
+
+      {isMenuOpen && (
+        <nav className="bg-white shadow-lg md:hidden">
+          <ul className="flex flex-col items-center space-y-4 py-4">
+            {navigation?.map(
+              (nav) =>
+                nav?.path && (
+                  <li key={nav.path}>
+                    <Link
+                      to={nav.path}
+                      className="hover:text-gray-600"
+                      onClick={toggleMenu} // Close menu on link click
+                    >
+                      {nav.name}
+                    </Link>
+                  </li>
+                ),
+            )}
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
