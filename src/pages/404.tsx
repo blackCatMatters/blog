644eb6839ef8f1eeb0870b64f34967f4da3c0668
afalco <20560604+bcm-afalco@users.noreply.github.com>
2024-12-28 16:46:49 +0100

    feat(content): add new articles for December 2024

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -33,9 +33,7 @@ const NotFoundPage: React.FC<PageProps> = () => {
         {process.env.NODE_ENV === 'development' ? (
           <>
             <br />
-            Try creating a page in <code style={codeStyles}>
-              src/pages/
-            </code>.
+            Try creating a page in <code style={codeStyles}>src/pages/</code>.
             <br />
           </>
         ) : null}
